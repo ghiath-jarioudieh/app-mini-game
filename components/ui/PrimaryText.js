914421f426d98dd853795dbdc41e653e3ef55ada
r.jairoudieh@readcoop.eu <r.jairoudieh@readcoop.eu>
2024-03-02 00:06:31 +0100

    Add primary text component

---
+++ components/ui/PrimaryText.js
@@ -0,0 +1,13 @@
+import { Text, StyleSheet } from "react-native";
+import Colors from "../../constants/colors";
+
+export default function PrimaryText({ children, style }) {
+  return <Text style={[styles.text, style]}>{children}</Text>;
+}
+
+const styles = StyleSheet.create({
+  text: {
+    color: Colors.accent500,
+    fontSize: 24,
+  },
+});
