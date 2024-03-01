c54171e7fa3d3ad4143a7c41f3f304b0fc3b1c84
r.jairoudieh@readcoop.eu <r.jairoudieh@readcoop.eu>
2024-03-01 21:07:04 +0100

    Implement number container

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,15 +1,29 @@
 import { View, Text, StyleSheet } from "react-native";
-import Title from "../components/Title";
+import Title from "../components/ui/Title";
+import { useState } from "react";
+import NumberContainer from "../components/game/NumberContainer";
 
-export default function GameScreen() {
+function createRandomNumber(min, max, exclude) {
+  const randomNumber = Math.floor(Math.random() * (max - min)) + min;
+
+  if (randomNumber === exclude) {
+    return createRandomNumber(min, max, exclude);
+  } else {
+    return randomNumber;
+  }
+}
+
+export default function GameScreen({ userNumber }) {
+  const initialGuess = createRandomNumber(1, 100, userNumber);
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
   return (
     <View style={styles.screen}>
       <Title>Opponent's Guess</Title>
+      <NumberContainer>{currentGuess}</NumberContainer>
+
       <View>
-        <Text>here</Text>
-      </View>
-      <View>
-        <Text>here</Text>
+        <Text>Higher or lower?</Text>
       </View>
     </View>
   );
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,7 +1,7 @@
 import { View, StyleSheet, TextInput, Alert } from "react-native";
 import { useState } from "react";
 
-import PrimaryButton from "../components/PrimaryButton";
+import PrimaryButton from "../components/ui/PrimaryButton";
 import Colors from "../constants/colors";
 
 export default function StartGameScreen({ onPickNumber }) {
